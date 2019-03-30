5ff0fa84a8735281b28d58789c279a6917ce6f4c
Jacz <jacz@jaczaus.me>
2019-03-30 21:08:22 +1100

    Update rethinkdb.js

--- providers/rethinkdb.js
+++ providers/rethinkdb.js
@@ -1,103 +1,98 @@
 // Copyright (c) 2017-2019 dirigeants. All rights reserved. MIT license.
-const { Provider, util: { mergeDefault, chunk } } = require('klasa');
-const rethink = require('rethinkdbdash');
+import { Provider, util } from 'klasa';
+import { MasterPool, r, TableChangeResult, WriteResult } from 'rethinkdb-ts';
 
-module.exports = class extends Provider {
+export default class RethinkDB extends Provider {
 
-	constructor(...args) {
-		super(...args);
-		this.db = rethink(mergeDefault({
+	public db = r;
+	public pool: MasterPool = null;
+
+	public async init(): Promise<void> {
+		const options = util.mergeDefault({
 			db: 'test',
-			user: 'admin',
-			password: '',
 			silent: false
-		}, this.client.options.providers.rethinkdb));
+		}, this.client.options.providers.rethinkdb);
+		this.pool = await r.connectPool(options);
+		await this.db.branch(this.db.dbList().contains(options.db), null, this.db.dbCreate(options.db)).run();
 	}
 
-	async init() {
-		const { db } = this.db._poolMaster._options;
-		await this.db.branch(this.db.dbList().contains(db), null, this.db.dbCreate(db)).run();
-	}
-
-	get exec() {
-		return this.db;
-	}
-
-	async ping() {
+	public async ping(): Promise<number> {
 		const now = Date.now();
-		return await this.db.now() - now;
+		return (await this.db.now().run()).getTime() - now;
 	}
 
-	async shutdown() {
-		return this.db.getPoolMaster().drain();
+	public shutdown(): Promise<void> {
+		return this.pool.drain();
 	}
 
 	/* Table methods */
 
-	async hasTable(table) {
+	public hasTable(table: string): Promise<boolean> {
 		return this.db.tableList().contains(table).run();
 	}
 
-	async createTable(table) {
+	public createTable(table: string): Promise<TableChangeResult> {
 		return this.db.tableCreate(table).run();
 	}
 
-	async deleteTable(table) {
+	public deleteTable(table: string): Promise<TableChangeResult> {
 		return this.db.tableDrop(table).run();
 	}
 
-	async sync(table) {
+	public sync(table: string): Promise<{ synced: number }> {
 		return this.db.table(table).sync().run();
 	}
 
 	/* Document methods */
 
-	async getAll(table, entries = []) {
+	public async getAll(table: string, entries: string[] = []): Promise<any[]> {
 		if (entries.length) {
-			const chunks = chunk(entries, 50000);
+			const chunks = util.chunk(entries, 50000);
 			const output = [];
+			// @ts-ignore
 			for (const myChunk of chunks) output.push(...await this.db.table(table).getAll(...myChunk).run());
 			return output;
 		}
 		return this.db.table(table).run();
 	}
 
-	async getKeys(table, entries = []) {
+	public async getKeys(table: string, entries: string[] = []): Promise<string[]> {
 		if (entries.length) {
-			const chunks = chunk(entries, 50000);
+			const chunks = util.chunk(entries, 50000);
 			const output = [];
+			// @ts-ignore
 			for (const myChunk of chunks) output.push(...await this.db.table(table).getAll(...myChunk)('id').run());
 			return output;
 		}
 		return this.db.table(table)('id').run();
 	}
 
-	async get(table, id) {
+	public get(table: string, id: string): Promise<any> {
 		return this.db.table(table).get(id).run();
 	}
 
-	async has(table, id) {
+	public has(table: string, id: string): Promise<boolean> {
 		return this.db.table(table).get(id).ne(null).run();
 	}
 
-	async getRandom(table) {
+	public getRandom(table: string): Promise<any> {
 		return this.db.table(table).sample(1).run();
 	}
 
-	async create(table, id, value = {}) {
+	public create(table: string, id: string, value: object = {}): Promise<WriteResult> {
 		return this.db.table(table).insert({ ...this.parseUpdateInput(value), id }).run();
 	}
 
-	async update(table, id, value = {}) {
+	public update(table: string, id: string, value: object = {}): Promise<WriteResult> {
 		return this.db.table(table).get(id).update(this.parseUpdateInput(value)).run();
 	}
 
-	async replace(table, id, value = {}) {
+	public replace(table: string, id: string, value: object = {}): Promise<WriteResult> {
 		return this.db.table(table).get(id).replace({ ...this.parseUpdateInput(value), id }).run();
 	}
 
-	async delete(table, id) {
+	public delete(table: string, id: string): Promise<WriteResult> {
 		return this.db.table(table).get(id).delete().run();
 	}
 
-};
+}
